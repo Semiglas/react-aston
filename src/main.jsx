7169abaa8d22861bb04d6b63714e7593dd55e661
Semiglas <congressmand@gmail.com>
2024-02-21 12:57:16 +0300

    created route

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HomePage from "./pages/HomePage";
+import store from "./store";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Header } from "./components/Header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Header/ >
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root"),
 );
